ac0fae3d2f8d893ec62cb8d5ee335ba5e991a7b8
 cambo2015 <cameronhansen91@outlook.com>
2019-09-11 05:21:23 -0600

    MoveCircle now has boundaries

--- js/3dObjects/MoveCircle.js
+++ js/3dObjects/MoveCircle.js
@@ -1,13 +1,37 @@
 class MoveCircle extends IAnimate {
   constructor(ctx) {
     super();
-    this.circle = new Circle(100, 100, 100, "green", ctx);
+    this.radius = 100
+    this.reverse = false
+    this.circle = new Circle(100, 100,this.radius, "green", ctx);
+  }
+  
+  
+  moveCircle(){
+    if(!this.reverse){
+      this.circle.position.x += 1
+    }
+    else if(this.reverse){
+      this.circle.position.x -= 1
+    }
+  }
+  checkReverse(){
+    if (this.circle.position.x >= window.innerWidth -this.radius-20){
+      this.reverse=true
+    }
+    else if(this.circle.position.x-this.radius <=0) {
+      this.reverse=false
+    }
   }
 
-  start() {}
+  start() {
+    this.circle.draw()
+  }
 
   update() {
-    this.circle.position.x += 1;
+    this.checkReverse()
+    this.moveCircle()
     this.circle.draw();
   }
+  
 }
